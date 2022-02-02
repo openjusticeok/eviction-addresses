352289bdba382d1dca7f277ed058392ca8141161
Brancen Gregory <brancengregory@gmail.com>
2022-02-02 18:46:05 -0500

    Interface and verification changes

--- dashboard/server.R
+++ dashboard/server.R
@@ -56,6 +56,8 @@ db <- dbConnect(
   project = "ojo-database",
   dataset = "ojo_eviction_addresses"
 )
+on.exit(dbDisconnect(db))
+
 
 function(input, output, session) {
   
@@ -107,6 +109,72 @@ function(input, output, session) {
     glue("Welcome {user_info()$name}")
   })
   
+  output$sidebar_menu <- renderMenu({
+    req(credentials()$user_auth)
+    sidebarMenu(
+      id = "sidebar-menu",
+      menuItem(
+        "Entry",
+        tabName = "entry",
+        icon = icon("edit")
+      ),
+      menuItem(
+        "Metrics",
+        tabName = "metrics",
+        icon = icon("chart-bar")
+      )
+    )
+  })
+  
+  output$entry_ui <- renderUI({
+    req(credentials()$user_auth)
+    tagList(
+      fluidRow(
+        column(
+          width = 4,
+          box(
+            width = 12,
+            uiOutput("current_case_ui"),
+            actionButton(
+              inputId = "case_refresh",
+              label = "Refresh",
+              icon = icon("sync")
+            )
+          )
+        ),
+        column(
+          width = 8,
+          offset = 0,
+          box(
+            width = 12,
+            uiOutput("address_entry_ui")
+          )
+        )
+      ),
+      fluidRow(
+        column(
+          width = 12,
+          box(
+            width = 12,
+            div(
+              style = "display: flex; justify-content: center; gap: 10px;",
+              actionButton(inputId = "previous_document", label = "Previous"),
+              textOutput("document_selector_ui"),
+              actionButton(inputId = "next_document", label = "Next")
+            ),
+            htmlOutput("current_document_ui")
+          )
+        )
+      )
+    )
+  })
+  
+  output$metrics_ui <- renderUI({
+    req(credentials()$user_auth)
+    
+    
+  })
+  
   output$testUI <- renderUI({
     req(credentials()$user_auth)
     
@@ -133,25 +201,12 @@ function(input, output, session) {
             width = 4,
             box(
               width = 12,
+              uiOutput("current_case_ui"),
               actionButton(
                 inputId = "case_refresh",
                 label = "Refresh",
                 icon = icon("sync")
-              ),
-              textOutput("current_case_ui")
-            )
-          ),
-          column(
-            width = 4,
-            box(
-              width = 12
-            )
-          ),
-          column(
-            width = 4,
-            offset = 0,
-            box(
-              uiOutput("address_validation_ui")
+              )
             )
           )
         ),
@@ -210,6 +265,20 @@ function(input, output, session) {
     # body <- content(res, "text")
     # return(body)
   })
+  
+  total_cases <- reactive({
+    input$case_refresh
+    
+    con <- dbConnect(
+      bigrquery::bigquery(),
+      project = "ojo-database",
+      dataset = "ojo_eviction_addresses"
+    )
+    on.exit(dbDisconnect(con))
+    
+    dbGetQuery(con, glue('SELECT COUNT(*) FROM `ojo_eviction_addresses.case` t LEFT JOIN `ojo-database.ojo_eviction_addresses.address` a ON t.id = a.case WHERE a.id IS NULL')) |>
+      pull()
+  })
 
   documents <- reactive({
     current_case <- current_case()
@@ -240,8 +309,7 @@ function(input, output, session) {
   )
   observeEvent(
     input$previous_document,
-    {message("Pressed previous")
-    if(current_document_num() <= 1) {
+    {if(current_document_num() <= 1) {
       current_document_num(total_documents())
     } else {
       current_document_num(current_document_num() - 1)
@@ -249,8 +317,7 @@ function(input, output, session) {
   )
   observeEvent(
     input$next_document,
-    {message("Pressed next")
-    if(current_document_num() >= total_documents()) {
+    {if(current_document_num() >= total_documents()) {
       current_document_num(1)
     } else {
       current_document_num(current_document_num() + 1)
@@ -260,14 +327,19 @@ function(input, output, session) {
   current_document <- reactive({
     documents <- documents()
     link <- documents[current_document_num(), "internal_link"]
-    message(link)
     return(link)
   })
 
-  output$current_case_ui <- renderText(str_c("Current Case: ", current_case()))
+  output$current_case_ui <- renderUI({
+    current_case <- fromJSON(current_case())
+    queue <- total_cases()
+    div(
+      h4(glue("Current case: {current_case$case_number}")),
+      h4(glue("{queue} cases in queue"))
+    )
+  })
 
   output$current_document_ui <- renderText({
-    #return(paste(current_document()))
     return(glue('<iframe style="height:600px; width:100%" src="', '{current_document()}', '"></iframe>'))
   })
 
@@ -280,40 +352,50 @@ function(input, output, session) {
   output$document_selector_ui <- renderText(str_c(current_document_num(), " / ", total_documents()))
   
   output$address_entry_ui <- renderUI({
-    tagList(
-      textInput(
-        inputId = "address_street_number",
-        label = "Street Number"
+    div(
+      div(
+        style = "display: flex; gap: 10px; justify-content: flex-start; flex-wrap: wrap;",
+        textInput(
+          inputId = "address_street_number",
+          label = "Street Number"
+        ),
+        textInput(
+          inputId = "address_street_direction",
+          label = "Street Direction"
+        ),
+        textInput(
+          inputId = "address_street_name",
+          label = "Street Name"
+        ),
+        textInput(
+          inputId = "address_street_type",
+          label = "Street Type"
+        )
       ),
-      textInput(
-        inputId = "address_street_direction",
-        label = "Street Direction"
+      div(
+        style = "display: flex; gap: 10px; justify-content: flex-start; flex-wrap: wrap;",
+        textInput(
+          inputId = "address_street_unit",
+          label = "APT/SUITE/UNIT..."
+        )
       ),
-      textInput(
-        inputId = "address_street_name",
-        label = "Street Name"
-      ),
-      textInput(
-        inputId = "address_street_type",
-        label = "Street Type"
-      ),
-      textInput(
-        inputId = "address_street_unit",
-        label = "APT/SUITE/UNIT..."
-      ),
-      textInput(
-        inputId = "address_city",
-        label = "City"
-      ),
-      selectInput(
-        inputId = "address_state",
-        label = "State",
-        choices = c("AR", "OK", "TX"),
-        selected = "OK"
-      ),
-      textInput(
-        inputId = "address_zip",
-        label = "Zip Code"
+      div(
+        style = "display: flex; gap: 10px; justify-content: flex-start; flex-wrap: wrap;",
+        textInput(
+          inputId = "address_city",
+          label = "City"
+        ),
+        selectInput(
+          width = "80px",
+          inputId = "address_state",
+          label = "State",
+          choices = c("AR", "OK", "TX"),
+          selected = "OK"
+        ),
+        textInput(
+          inputId = "address_zip",
+          label = "Zip Code"
+        )
       ),
       actionButton(
         inputId = "address_validate",
@@ -327,32 +409,72 @@ function(input, output, session) {
       )
     )
   })
-
-  output$address_validation_ui <- renderUI({
-    input$address_validate
-
-    token <- cr_jwt_token(jwt, api_url)
-    url <- str_c(api_url, "/address/validate")
-
-    res <- cr_jwt_with_httr(
-      POST(
-        url,
-        body = list(
-          street_num = isolate(input$address_street_number),
-          street_dir = isolate(input$address_street_direction),
-          street_name = isolate(input$address_street_name),
-          street_type = isolate(input$address_street_type),
-          unit = isolate(input$address_street_unit),
-          city = isolate(input$address_city),
-          state = isolate(input$address_state),
-          zip = isolate(input$address_zip)
-        ),
-        encode = "json"
-      ),
-      token
+  
+  observeEvent(input$address_validate, {
+    
+    address_entered <- list(
+      street_num = isolate(input$address_street_number),
+      street_direction = isolate(input$address_street_direction),
+      street_name = isolate(input$address_street_name),
+      street_type = isolate(input$address_street_type),
+      unit = isolate(input$address_street_unit),
+      city = isolate(input$address_city),
+      state = isolate(input$address_state),
+      zip = isolate(input$address_zip)
     )
-    validated <- content(res, as = "parsed", encoding = "UTF-8")
-
-    renderText(c("Validated: ", as.character(validated[[1]]$Address2)))
+    
+    address_entered_string <- str_c(address_entered$street_num,
+                                    " ",
+                                    address_entered$street_direction,
+                                    " ",
+                                    address_entered$street_name,
+                                    " ",
+                                    address_entered$street_type,
+                                    " ",
+                                    address_entered$street_unit,
+                                    " ",
+                                    address_entered$city,
+                                    ", ",
+                                    address_entered$state,
+                                    " ",
+                                    address_entered$zip)
+    
+    showModal(modalDialog(
+      title = "Confirm",
+      h5("Are you sure you want to submit this address?"),
+      div(
+        h5("Address entered:"),
+        h5(address_entered_string)
+      ),
+      div(
+        p(glue("Verified address:"))
+      )
+    ))
+    
+    # input$address_validate
+    # 
+    # token <- cr_jwt_token(jwt, api_url)
+    # url <- str_c(api_url, "/address/validate")
+    # 
+    # res <- cr_jwt_with_httr(
+    #   POST(
+    #     url,
+    #     body = list(
+    #       street_num = isolate(input$address_street_number),
+    #       street_dir = isolate(input$address_street_direction),
+    #       street_name = isolate(input$address_street_name),
+    #       street_type = isolate(input$address_street_type),
+    #       unit = isolate(input$address_street_unit),
+    #       city = isolate(input$address_city),
+    #       state = isolate(input$address_state),
+    #       zip = isolate(input$address_zip)
+    #     ),
+    #     encode = "json"
+    #   ),
+    #   token
+    # )
+    # validated <- content(res, as = "parsed", encoding = "UTF-8")
+    # 
+    # renderText(c("Validated: ", as.character(validated[[1]]$Address2)))
   })
 }
