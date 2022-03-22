ddd29d966b4df9503788e9fc46087c375a5413b3
Brancen Gregory <brancengregory@gmail.com>
2022-03-22 15:07:53 -0400

    Remove bq comments

--- api/plumber.R
+++ api/plumber.R
@@ -46,15 +46,11 @@ ojodb <- pool::dbPool(odbc::odbc(),
                       )
 )
 
-#minute_table <- in_schema("eviction_addresses", "tulsa_eviction_minutes")
-#document_table <- in_schema("eviction-addresses", "document")
-
 #res <- ojodb |>
 #	dbGetQuery("select * from pg_stat_ssl where pid = pg_backend_pid();")
 
 #cli_alert_info("{res}")
 
-#message(bq_user())
 
 #* Refreshes materialized views
 #* @get /refresh
