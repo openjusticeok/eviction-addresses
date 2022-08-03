1aab8424794623002ced651685a5f5f09284e1d6
Brancen Gregory <brancengregory@gmail.com>
2022-08-03 16:01:52 -0500

    Modify demo map

--- R/demo_map.R
+++ R/demo_map.R
@@ -1,15 +1,23 @@
 library(ojodb)
 library(sf)
 library(tmap)
-library(spData)
+library(tigris)
 
-ok <- us_states |>
+tmap_mode("plot")
+
+ok_counties <- counties("Oklahoma", cb = TRUE)
+tulsa_county <- ok_counties |>
+  filter(NAME == "Tulsa")
+
+ok <- states() |>
   filter(NAME == "Oklahoma")
 
 proj4 <- st_crs(ok)$proj4string
 
 data <- ojo_tbl("address", "eviction_addresses") |>
-  select(case, lat, lon, geo_accuracy) |>
+  select(case, lat, lon, geo_accuracy, geo_accuracy_type) |>
+  left_join(ojo_tbl("case") |>
+              select(id, district, date_filed), by = c("case" = "id")) |>
   collect()
 
 data_points <- data |>
@@ -18,10 +26,14 @@ data_points <- data |>
 
 tm_shape(ok) +
   tm_polygons('#f0f0f0f0', border.alpha = 0.2) +
-  tm_shape(data_points) +
+  tm_shape(data_points |>
+             filter(geo_accuracy >= 0.9)) +
   tm_dots(alpha = 0.2)
 
-
-
+tm_shape(tulsa_county) +
+  tm_polygons('#f0f0f0f0', border.alpha = 0.2) +
+  tm_shape(data_points |>
+             filter(geo_accuracy >= 0.9)) +
+  tm_dots(alpha = 0.2)
 
 
