97f8cb34e1f229efe497e80b1e847a780151f718
Brancen Gregory <brancengregory@gmail.com>
2022-09-06 12:17:09 -0400

    Add vcr

--- tests/testthat/test-mturk.R
+++ tests/testthat/test-mturk.R
@@ -1,2 +1,15 @@
-# test_that("MTurk auth fails on no config or env variables", {
-# })
+logger::log_threshold(FATAL)
+
+test_that("MTurk auth fails on no config or env variables", {
+  withr::local_envvar(
+    list(
+      AWS_ACCESS_KEY_ID = NULL,
+      AWS_SECRET_ACCESS_KEY = NULL
+    )
+  )
+
+  res <- auth_mturk()
+
+  expect_false(res)
+})
+
