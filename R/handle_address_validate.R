e980c0e59542a6aeae0566c5ca34060ea0c14c48
Brancen Gregory <brancengregory@gmail.com>
2022-09-12 14:51:22 -0400

    Add address validate api endpoint handler func

--- R/handle_address_validate.R
+++ R/handle_address_validate.R
@@ -1,3 +1,11 @@
+#' @title Handle Address Validate
+#'
+#' @param db A database connection pool created by `pool::dbPool()`
+#' @param config The path to a `config.yml` file to be ingested by `{config}`
+#'
+#' @return A handler function for route /address/validate
+#' @export
+#'
 handle_address_validate <- function(db, config) {
   f <- function(address) {
     res <- send_postgrid_request(config = config, address = address)
