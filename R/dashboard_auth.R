06ab85dea9f88dcebfba5b46ad9ad457889c5d18
Brancen Gregory <brancengregory@gmail.com>
2022-09-21 15:18:11 -0400

    consolidate case, minute, etc

---
+++ R/dashboard_auth.R
@@ -0,0 +1,68 @@
+#' @title Get Sessions from DB
+#' @description Gets a tibble of sessions from the database for use by {shinyauth}
+#'
+#' @param conn The database connection
+#' @param cookie_expiry The cookie expiration
+#'
+#' @return A tibble of session info
+#'
+get_sessions_from_db <- function(conn, cookie_expiry = 7) {
+  f <- function(expiry = cookie_expiry) {
+    logger::log_debug("Getting sessions from db")
+    DBI::dbGetQuery(
+      conn = conn,
+      dbplyr::sql('SELECT * FROM "eviction_addresses"."session"')
+    ) |>
+      dplyr::mutate(login_time = lubridate::ymd_hms(.data$login_time)) |>
+      tibble::as_tibble() |>
+      dplyr::filter(.data$login_time > lubridate::now(tzone = "America/Chicago") - lubridate::days(cookie_expiry))
+  }
+  return(f)
+}
+
+
+#' @title Add Session to DB
+#' @description Adds a session to the database for {shinyauth}
+#'
+#' @param db A database connection Pool
+#'
+#' @return Returns invisibly if successful
+#'
+add_session_to_db <- function(db) {
+  f <- function(user, sessionid, conn) {
+    logger::log_debug("Adding session to db")
+    values <- tibble::tibble(
+      user = user,
+      sessionid = sessionid,
+      login_time = as.character(lubridate::now(tzone = "America/Chicago")))
+    logger::log_trace("{values}")
+    res <- DBI::dbWriteTable(
+      conn = db,
+      name = DBI::Id(schema = "eviction_addresses", table = "session"),
+      value = values,
+      append = TRUE,
+      row.names = F
+    )
+    logger::log_debug("Wrote session to database table 'session'")
+  }
+}
+
+
+#' @title Get Users from DB
+#' @description Gets a tibble of users from the database to be used by {shinyauth}
+#'
+#' @param db A database connection pool created with `pool::dbPool`
+#'
+#' @return A tibble of user info
+#'
+get_users_from_db <- function(db) {
+  logger::log_debug("Getting users from db")
+  DBI::dbGetQuery(
+    db,
+    dbplyr::sql('SELECT * FROM "eviction_addresses"."user"')
+  ) |>
+    tibble::as_tibble()
+}
+
+
+
--- R/document.R
+++ R/document.R
@@ -50,3 +50,19 @@ refresh_documents <- function(db) {
 
   return()
 }
+
+
+#' @title Get Documents by Case
+#'
+#' @param db A database connection pool created with `pool::dbPool`
+#' @param id The id of the case for which to return documents
+#'
+#' @return A data.frame of documents
+#'
+get_documents_by_case <- function(db, id) {
+  query <- glue::glue_sql('SELECT * FROM "eviction_addresses"."document" t WHERE t."case" = {id};', .con = db)
+
+  res <- DBI::dbGetQuery(db, query)
+
+  return(res)
+}
