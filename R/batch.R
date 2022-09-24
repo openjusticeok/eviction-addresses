b9ed8f67670f227faa921d021b7d265d68905f36
Brancen Gregory <brancengregory@gmail.com>
2022-09-23 20:40:36 -0400

    Add case batch

--- R/batch.R
+++ R/batch.R
@@ -2,12 +2,12 @@
 #'
 #' @param db
 #' @param max_batch_size
+#' @param hit_type
 #'
 #' @return
 #' @export
 #'
-#' @examples
-new_mturk_batch <- function(db, max_batch_size) {
+new_mturk_batch <- function(db, max_batch_size, hit_type) {
   assert_that(
     is.number(max_batch_size),
     is.integer(max_batch_size)
@@ -16,11 +16,31 @@ new_mturk_batch <- function(db, max_batch_size) {
   queue_length <- get_queue_length(db)
   if(queue_length > 0) {
     up_limit <- min(queue_length, max_batch_size)
-    for(i in 1:up_limit) {
-      c <- new_case_from_queue(db)
-      new_hit_from_case(db, c)
-    }
+    purrr::walk(
+      seq_len(up_limit),
+      purrr::safely(
+        batch_case(db, hit_type)
+      )
+    )
   }
+
+  return()
 }
+
+#' Title
+#'
+#' @param db
+#' @param hit_type
+#'
+#' @return
+#' @export
+#'
+#' @examples
+batch_case <- function(db, hit_type) {
+  c <- new_case_from_queue(db)
+  h <- new_hit_from_case(db = db, case = c, hit_type = hit_type)
+}
+
+
 
 
