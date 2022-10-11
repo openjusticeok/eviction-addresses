20ef8197eea977b1fc3755bae0a42aa42608451d
Brancen Gregory <brancengregory@gmail.com>
2022-10-11 15:57:37 -0400

    test

--- inst/cloudbuild/build_test.R
+++ inst/cloudbuild/build_test.R
@@ -37,9 +37,9 @@ eviction_addresses_api_yaml <- cr_build_yaml(
       decrypted = "/workspace/shiny-apps-certs/server-ca.pem"
     ),
     cr_buildstep_bash("chmod 0600 /workspace/shiny-apps-certs/client-key.pem"),
+    cr_buildstep_bash("cp inst/cloudbuild/Dockerfile ."),
     cr_buildstep_docker(
       image = "eviction-addresses-api-test",
-      dir = "inst/cloudbuild",
       kaniko_cache = F
     ),
     cr_buildstep_run(
