2987281f1aa4001db037a3c796cf58705521a957
Brancen Gregory <brancengregory@gmail.com>
2022-12-13 23:19:06 -0600

    Update build files

--- inst/cloudbuild/build.R
+++ inst/cloudbuild/build.R
@@ -1,13 +1,5 @@
 library(googleCloudRunner)
 
-#library(containerit)
-#
-# api_dockerfile <- containerit::dockerfile(from = "api/plumber.R",
-#                                           filter_baseimage_pkgs = T)
-#
-# write(api_dockerfile, file = "api/Dockerfile")
-
-
 eviction_addresses_api_yaml <- cr_build_yaml(
   steps = c(
     cr_buildstep_secret(
@@ -39,7 +31,7 @@ eviction_addresses_api_yaml <- cr_build_yaml(
     cr_buildstep_bash("cp inst/cloudbuild/Dockerfile ."),
     cr_buildstep_docker(
       image = "eviction-addresses-api",
-      kaniko_cache = F
+      kaniko_cache = FALSE
     ),
     cr_buildstep_run(
       name = "eviction-addresses-api",
@@ -49,7 +41,7 @@ eviction_addresses_api_yaml <- cr_build_yaml(
       cpu = 1,
       max_instances = 1,
       concurrency = 80,
-      allowUnauthenticated = F,
+      allowUnauthenticated = FALSE,
       gcloud_args = c("--timeout=3600")
     )
   ),
@@ -75,91 +67,75 @@ cr_buildtrigger(
 )
 
 
-# dashboard_dockerfile <- containerit::dockerfile(
-#   image = "rocker/shiny",
-#   from = "dashboard/server.R",
-#   filter_baseimage_pkgs = T
-# )
-#
-# write(dashboard_dockerfile, file = "dashboard/sample-Dockerfile")
+eviction_addresses_dashboard_yaml <- cr_build_yaml(
+  steps = c(
+    cr_buildstep_secret(
+      secret = "eviction-addresses-client-id",
+      decrypted = "dashboard/client-id.json"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-dashboard-renviron",
+      decrypted = "dashboard/.Renviron"
+    ),
+    # cr_buildstep_secret(
+    #   secret = "eviction-addresses-dashboard-service-account",
+    #   decrypted = "dashboard/eviction-addresses-dashboard-service-account.json"
+    # ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-service-account",
+      decrypted = "dashboard/eviction-addresses-service-account.json"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-api-config",
+      decrypted = "dashboard/config.yml"
+    ),
+    cr_buildstep_bash("mkdir -p dashboard/shiny-apps-certs/"),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-ssl-cert",
+      decrypted = "dashboard/shiny-apps-certs/client-cert.pem"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-ssl-key",
+      decrypted = "dashboard/shiny-apps-certs/client-key.pem"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-ssl-ca",
+      decrypted = "dashboard/shiny-apps-certs/server-ca.pem"
+    ),
+    cr_buildstep_bash("chmod 0600 dashboard/shiny-apps-certs/client-key.pem"),
+    cr_buildstep_docker(
+      image = "eviction-addresses-dashboard",
+      dir = "dashboard",
+      kaniko_cache = T
+    ),
+    cr_buildstep_run(
+      name = "eviction-addresses-dashboard",
+      image = "gcr.io/ojo-database/eviction-addresses-dashboard:$BUILD_ID",
+      port = 3838,
+      memory = "1G",
+      cpu = 1,
+      max_instances = 1,
+      concurrency = 80
+    )
+  ),
+  timeout = 7200
+)
+eviction_addresses_dashboard_build <- cr_build_make(
+  yaml = eviction_addresses_dashboard_yaml
+)
+eviction_addresses_dashboard_trigger <- cr_buildtrigger_repo(
+  repo_name = "openjusticeok/eviction-addresses",
+  branch = "main"
+)
+cr_buildtrigger_delete("eviction-addresses-dashboard-trigger")
+cr_buildtrigger(
+  build = eviction_addresses_dashboard_build,
+  name = "eviction-addresses-dashboard-trigger",
+  trigger = eviction_addresses_dashboard_trigger,
+  includedFiles = "**"
+)
 
-
-# eviction_addresses_dashboard_yaml <- cr_build_yaml(
-#   steps = c(
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-client-id",
-#       decrypted = "dashboard/client-id.json"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-dashboard-renviron",
-#       decrypted = "dashboard/.Renviron"
-#     ),
-#     # cr_buildstep_secret(
-#     #   secret = "eviction-addresses-dashboard-service-account",
-#     #   decrypted = "dashboard/eviction-addresses-dashboard-service-account.json"
-#     # ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-service-account",
-#       decrypted = "dashboard/eviction-addresses-service-account.json"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-api-config",
-#       decrypted = "dashboard/config.yml"
-#     ),
-#     cr_buildstep_bash("mkdir -p dashboard/shiny-apps-certs/"),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-ssl-cert",
-#       decrypted = "dashboard/shiny-apps-certs/client-cert.pem"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-ssl-key",
-#       decrypted = "dashboard/shiny-apps-certs/client-key.pem"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-ssl-ca",
-#       decrypted = "dashboard/shiny-apps-certs/server-ca.pem"
-#     ),
-#     cr_buildstep_bash("chmod 0600 dashboard/shiny-apps-certs/client-key.pem"),
-#     cr_buildstep_docker(
-#       image = "eviction-addresses-dashboard",
-#       dir = "dashboard",
-#       kaniko_cache = T
-#     ),
-#     cr_buildstep_run(
-#       name = "eviction-addresses-dashboard",
-#       image = "gcr.io/ojo-database/eviction-addresses-dashboard:$BUILD_ID",
-#       port = 3838,
-#       memory = "1G",
-#       cpu = 1,
-#       max_instances = 1,
-#       concurrency = 80
-#     )
-#   ),
-#   timeout = 7200
-# )
-#
-# eviction_addresses_dashboard_build <- cr_build_make(
-#   yaml = eviction_addresses_dashboard_yaml
-# )
-#
-# eviction_addresses_dashboard_trigger <- cr_buildtrigger_repo(
-#   repo_name = "openjusticeok/eviction-addresses",
-#   branch = "main"
-# )
-#
-# cr_buildtrigger_delete("eviction-addresses-dashboard-trigger")
-#
-# cr_buildtrigger(
-#   build = eviction_addresses_dashboard_build,
-#   name = "eviction-addresses-dashboard-trigger",
-#   trigger = eviction_addresses_dashboard_trigger,
-#   includedFiles = "**"
-# )
-#
-#
 # ###### Test JWT generation ##########
-#
-
  cr <- cr_run_get("eviction-addresses-api")
  url <- cr$status$url
  jwt <- cr_jwt_create(url)
--- inst/cloudbuild/build_test.R
+++ inst/cloudbuild/build_test.R
@@ -1,13 +1,5 @@
 library(googleCloudRunner)
 
-#library(containerit)
-#
-# api_dockerfile <- containerit::dockerfile(from = "api/plumber.R",
-#                                           filter_baseimage_pkgs = T)
-#
-# write(api_dockerfile, file = "api/Dockerfile")
-
-
 eviction_addresses_api_yaml <- cr_build_yaml(
   steps = c(
     cr_buildstep_secret(
@@ -75,90 +67,79 @@ cr_buildtrigger(
 )
 
 
-# dashboard_dockerfile <- containerit::dockerfile(
-#   image = "rocker/shiny",
-#   from = "dashboard/server.R",
-#   filter_baseimage_pkgs = T
-# )
-#
-# write(dashboard_dockerfile, file = "dashboard/sample-Dockerfile")
+eviction_addresses_dashboard_yaml <- cr_build_yaml(
+  steps = c(
+    cr_buildstep_secret(
+      secret = "eviction-addresses-client-id",
+      decrypted = "dashboard/client-id.json"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-dashboard-renviron",
+      decrypted = "dashboard/.Renviron"
+    ),
+    # cr_buildstep_secret(
+    #   secret = "eviction-addresses-dashboard-service-account",
+    #   decrypted = "dashboard/eviction-addresses-dashboard-service-account.json"
+    # ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-service-account",
+      decrypted = "dashboard/eviction-addresses-service-account.json"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-api-config",
+      decrypted = "dashboard/config.yml"
+    ),
+    cr_buildstep_bash("mkdir -p dashboard/shiny-apps-certs/"),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-ssl-cert",
+      decrypted = "dashboard/shiny-apps-certs/client-cert.pem"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-ssl-key",
+      decrypted = "dashboard/shiny-apps-certs/client-key.pem"
+    ),
+    cr_buildstep_secret(
+      secret = "eviction-addresses-ssl-ca",
+      decrypted = "dashboard/shiny-apps-certs/server-ca.pem"
+    ),
+    cr_buildstep_bash("chmod 0600 dashboard/shiny-apps-certs/client-key.pem"),
+    cr_buildstep_docker(
+      image = "eviction-addresses-dashboard-test",
+      dir = "dashboard",
+      kaniko_cache = T
+    ),
+    cr_buildstep_run(
+      name = "eviction-addresses-dashboard-test",
+      image = "gcr.io/ojo-database/eviction-addresses-dashboard-test:$BUILD_ID",
+      port = 3838,
+      memory = "1G",
+      cpu = 1,
+      max_instances = 1,
+      concurrency = 80
+    )
+  ),
+  timeout = 7200
+)
 
+eviction_addresses_dashboard_build <- cr_build_make(
+  yaml = eviction_addresses_dashboard_yaml
+)
 
-# eviction_addresses_dashboard_yaml <- cr_build_yaml(
-#   steps = c(
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-client-id",
-#       decrypted = "dashboard/client-id.json"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-dashboard-renviron",
-#       decrypted = "dashboard/.Renviron"
-#     ),
-#     # cr_buildstep_secret(
-#     #   secret = "eviction-addresses-dashboard-service-account",
-#     #   decrypted = "dashboard/eviction-addresses-dashboard-service-account.json"
-#     # ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-service-account",
-#       decrypted = "dashboard/eviction-addresses-service-account.json"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-api-config",
-#       decrypted = "dashboard/config.yml"
-#     ),
-#     cr_buildstep_bash("mkdir -p dashboard/shiny-apps-certs/"),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-ssl-cert",
-#       decrypted = "dashboard/shiny-apps-certs/client-cert.pem"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-ssl-key",
-#       decrypted = "dashboard/shiny-apps-certs/client-key.pem"
-#     ),
-#     cr_buildstep_secret(
-#       secret = "eviction-addresses-ssl-ca",
-#       decrypted = "dashboard/shiny-apps-certs/server-ca.pem"
-#     ),
-#     cr_buildstep_bash("chmod 0600 dashboard/shiny-apps-certs/client-key.pem"),
-#     cr_buildstep_docker(
-#       image = "eviction-addresses-dashboard-test",
-#       dir = "dashboard",
-#       kaniko_cache = T
-#     ),
-#     cr_buildstep_run(
-#       name = "eviction-addresses-dashboard-test",
-#       image = "gcr.io/ojo-database/eviction-addresses-dashboard-test:$BUILD_ID",
-#       port = 3838,
-#       memory = "1G",
-#       cpu = 1,
-#       max_instances = 1,
-#       concurrency = 80
-#     )
-#   ),
-#   timeout = 7200
-# )
-#
-# eviction_addresses_dashboard_build <- cr_build_make(
-#   yaml = eviction_addresses_dashboard_yaml
-# )
-#
-# eviction_addresses_dashboard_trigger <- cr_buildtrigger_repo(
-#   repo_name = "openjusticeok/eviction-addresses",
-#   branch = "dev"
-# )
-#
-# cr_buildtrigger_delete("eviction-addresses-dashboard-test-trigger")
-#
-# cr_buildtrigger(
-#   build = eviction_addresses_dashboard_build,
-#   name = "eviction-addresses-dashboard-test-trigger",
-#   trigger = eviction_addresses_dashboard_trigger,
-#   includedFiles = "**"
-# )
-#
-#
-# ###### Test JWT generation ##########
-#
+eviction_addresses_dashboard_trigger <- cr_buildtrigger_repo(
+  repo_name = "openjusticeok/eviction-addresses",
+  branch = "test"
+)
+
+cr_buildtrigger_delete("eviction-addresses-dashboard-test-trigger")
+
+cr_buildtrigger(
+  build = eviction_addresses_dashboard_build,
+  name = "eviction-addresses-dashboard-test-trigger",
+  trigger = eviction_addresses_dashboard_trigger,
+  includedFiles = "**"
+)
+
+###### Test JWT generation ##########
  cr <- cr_run_get("eviction-addresses-api-test")
  url <- cr$status$url
  jwt <- cr_jwt_create(url)
