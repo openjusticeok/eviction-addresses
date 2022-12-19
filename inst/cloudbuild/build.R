3893fe630e6e50a4bd9edfddbc580c3e1fc6242e
Brancen Gregory <brancengregory@gmail.com>
2022-12-18 21:44:36 -0600

    Modify main build

--- inst/cloudbuild/build.R
+++ inst/cloudbuild/build.R
@@ -28,7 +28,7 @@ eviction_addresses_api_yaml <- cr_build_yaml(
       decrypted = "/workspace/shiny-apps-certs/server-ca.pem"
     ),
     cr_buildstep_bash("chmod 0600 /workspace/shiny-apps-certs/client-key.pem"),
-    cr_buildstep_bash("cp inst/cloudbuild/Dockerfile ."),
+    cr_buildstep_bash("cp inst/cloudbuild/Dockerfile ./Dockerfile"),
     cr_buildstep_docker(
       image = "eviction-addresses-api",
       kaniko_cache = FALSE
@@ -71,42 +71,42 @@ eviction_addresses_dashboard_yaml <- cr_build_yaml(
   steps = c(
     cr_buildstep_secret(
       secret = "eviction-addresses-client-id",
-      decrypted = "dashboard/client-id.json"
+      decrypted = "/workspace/client-id.json"
     ),
     cr_buildstep_secret(
       secret = "eviction-addresses-dashboard-renviron",
-      decrypted = "dashboard/.Renviron"
+      decrypted = "/workspace/.Renviron"
     ),
     # cr_buildstep_secret(
     #   secret = "eviction-addresses-dashboard-service-account",
-    #   decrypted = "dashboard/eviction-addresses-dashboard-service-account.json"
+    #   decrypted = "/workspace/eviction-addresses-dashboard-service-account.json"
     # ),
     cr_buildstep_secret(
       secret = "eviction-addresses-service-account",
-      decrypted = "dashboard/eviction-addresses-service-account.json"
+      decrypted = "/workspace/eviction-addresses-service-account.json"
     ),
     cr_buildstep_secret(
       secret = "eviction-addresses-api-config",
-      decrypted = "dashboard/config.yml"
+      decrypted = "/workspace/config.yml"
     ),
-    cr_buildstep_bash("mkdir -p dashboard/shiny-apps-certs/"),
+    cr_buildstep_bash("mkdir -p /workspace/shiny-apps-certs/"),
     cr_buildstep_secret(
       secret = "eviction-addresses-ssl-cert",
-      decrypted = "dashboard/shiny-apps-certs/client-cert.pem"
+      decrypted = "/workspace/shiny-apps-certs/client-cert.pem"
     ),
     cr_buildstep_secret(
       secret = "eviction-addresses-ssl-key",
-      decrypted = "dashboard/shiny-apps-certs/client-key.pem"
+      decrypted = "/workspace/shiny-apps-certs/client-key.pem"
     ),
     cr_buildstep_secret(
       secret = "eviction-addresses-ssl-ca",
-      decrypted = "dashboard/shiny-apps-certs/server-ca.pem"
+      decrypted = "/workspace/shiny-apps-certs/server-ca.pem"
     ),
-    cr_buildstep_bash("chmod 0600 dashboard/shiny-apps-certs/client-key.pem"),
+    cr_buildstep_bash("chmod 0600 /workspace/shiny-apps-certs/client-key.pem"),
+    cr_buildstep_bash("cp inst/cloudbuild/shiny_Dockerfile ./Dockerfile"),
     cr_buildstep_docker(
       image = "eviction-addresses-dashboard",
-      dir = "dashboard",
-      kaniko_cache = T
+      kaniko_cache = FALSE
     ),
     cr_buildstep_run(
       name = "eviction-addresses-dashboard",
@@ -120,14 +120,18 @@ eviction_addresses_dashboard_yaml <- cr_build_yaml(
   ),
   timeout = 7200
 )
+
 eviction_addresses_dashboard_build <- cr_build_make(
   yaml = eviction_addresses_dashboard_yaml
 )
+
 eviction_addresses_dashboard_trigger <- cr_buildtrigger_repo(
   repo_name = "openjusticeok/eviction-addresses",
   branch = "main"
 )
+
 cr_buildtrigger_delete("eviction-addresses-dashboard-trigger")
+
 cr_buildtrigger(
   build = eviction_addresses_dashboard_build,
   name = "eviction-addresses-dashboard-trigger",
@@ -135,16 +139,24 @@ cr_buildtrigger(
   includedFiles = "**"
 )
 
-# ###### Test JWT generation ##########
- cr <- cr_run_get("eviction-addresses-api")
- url <- cr$status$url
- jwt <- cr_jwt_create(url)
+###### Test JWT generation ##########
+cr <- cr_run_get("eviction-addresses-api")
+url <- cr$status$url
+jwt <- cr_jwt_create(url)
+token <- cr_jwt_token(jwt, url)
 
- library(httr)
- library(stringr)
- token <- cr_jwt_token(jwt, url)
- res <- cr_jwt_with_httr(
-   GET(str_c(url, "/mturk/review")),
-   token
- )
- content(res)
+library(httr)
+library(stringr)
+res <- cr_jwt_with_httr(
+  GET(str_c(url, "/dbpingfuture")),
+  token
+)
+content(res)
+
+b <- format_postgrid_request(
+  line1 = "13243 S 68 E AVE",
+  city = "Bixby",
+  state = "OK",
+  zip = "74008"
+)
+res <- send_postgrid_request("config.yml", b, geocode = TRUE)
