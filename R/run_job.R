09af754134a1271e05e0102900b937308a4b907e
Brancen Gregory <brancengregory@gmail.com>
2022-12-23 14:33:00 -0600

    Add log and export run_job

--- R/run_job.R
+++ R/run_job.R
@@ -4,7 +4,12 @@
 #' 
 #' @param config The path to a configuration file ingested by `{config}`
 #' 
+#' @export
+#' @returns Nothing
+#' 
 run_job <- function(config) {
+    logger::log_threshold(logger::TRACE)
+
     db <- new_db_pool(config)
     withr::defer(pool::poolClose(db))
 
