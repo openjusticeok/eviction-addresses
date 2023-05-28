bf53487c8b9681a2e0c67a9dc6398b15abff934f
Brancen Gregory <brancengregory@gmail.com>
2023-05-28 16:14:59 -0400

    Reporting (#48)

--- inst/scripts/check_pdfs.R
+++ inst/scripts/check_pdfs.R
@@ -2,34 +2,47 @@ library(googleCloudStorageR)
 library(googleCloudRunner)
 library(tidyverse)
 
-gcs_auth()
+# Authenticate with Google Cloud Storage
+gcs_auth(json_file = "eviction-addresses-service-account.json")
 
-docs <- googleCloudStorageR::gcs_list_objects("eviction-addresses")
+# Get a list of all the documents in the bucket
+docs <- googleCloudStorageR::gcs_list_objects("eviction-addresses") |>
+  as_tibble()
 
+# Filter for documents that are too small to be valid PDFs
 empties <- docs |>
-  as_tibble() |>
   mutate(size_num = str_extract(size, "\\d+\\.\\d+") |>
            as.numeric()) |>
-  filter(size_num == 1.2, str_detect(size, "Kb")) |>
-  pull(name)
+  filter(size_num == 1.2, str_detect(size, "Kb"))
 
-# walk(empties, function(x){print(x); gcs_delete_object(x, bucket = "eviction-addresses")})
+# Delete them
+walk(
+  empties$name,
+  function(x) {
+    gcs_delete_object(x, bucket = "eviction-addresses")
+  }
+)
 
-# for (i in seq_along(docs)) {
-#   url <- gcs_download_url(
-#     object_name = docs$name[i],
-#     bucket = "eviction_addresses",
-#     public = TRUE
-#   )
-#
-#   file <- tempfile()
-#   res <- httr::GET(
-#     url = url
-#   )
-#
-#   if(!res$status_code == 200) {
-#     next()
-#   }
-#
-#   readr::write_file(x = res$content, file = file, append = FALSE)
-# }
+# Get a list of all the documents in the database
+records <- ojo_tbl("document", "eviction_addresses") |>
+  filter(!is.na(internal_link)) |>
+  ojo_collect()
+
+# Find the documents that are in the database but not in the bucket
+deads <- anti_join(
+  records,
+  docs,
+  by = c("id" = "name")
+)
+
+# Delete their internal link from the database since it is broken
+walk(
+  deads$id,
+  function(x) {
+    q <- glue_sql(
+      .con = ojo_connect(),
+      'UPDATE "eviction_addresses"."document" SET "internal_link" = NULL WHERE "id" = {x};'
+    )
+    dbExecute(ojo_connect(), q)
+  }
+)
