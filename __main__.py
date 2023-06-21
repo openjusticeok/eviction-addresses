114f02c52ca5412f840f5a93366a9a3942f8d711
Brancen Gregory <brancengregory@gmail.com>
2023-06-20 22:42:26 -0400

    WIP

--- __main__.py
+++ __main__.py
@@ -1,33 +1,73 @@
-"""A Google Cloud Python Pulumi program"""
-
 import pulumi
-from pulumi_gcp import storage, projects, cloudbuild, compute
+import pulumi_gcp as gcp
+import pulumi_docker as docker
 
-# Create a new GCP project
-project = projects.Project('eviction-addresses')
+gcp_config = pulumi.Config('gcp')
+project = gcp_config.require('project')
 
-# Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket', location="US")
-
-# Export the DNS name of the bucket
-pulumi.export('bucket_name', bucket.url)
-
-# Create a Google Cloud Build trigger
-api_trigger = cloudbuild.Trigger('eviction-addresses-api-trigger',
-  description='A trigger for the eviction-addresses-api',
-  github={
-    'owner': 'openjusticeok',
-    'name': 'eviction-addresses',
-    'push': {
-      'branch': 'main',
-    },
-  },
-  substitutions={
-    'bucket_name': bucket.name,
-  },
-  filename='cloudbuild.yaml',
+# Create a service account
+service_account = gcp.serviceaccount.Account(
+    'eviction-addresses-service-account',
+    account_id='eviction-addresses-sa'
 )
 
-dashboard_trigger = cloudbuild.Trigger('eviction-addresses-dashboard-trigger')
+# Grant the service account the necessary permissions
+storage_admin_binding = gcp.projects.IAMBinding(
+    'eviction-addresses-storage-admin',
+    role="roles/storage.admin",
+    members=[pulumi.Output.concat("serviceAccount:", service_account.email)],
+    project=project
+)
 
-job_trigger = cloudbuild.Trigger('my-job-trigger')
+artifact_admin_binding = gcp.projects.IAMBinding(
+    'eviction-addresses-artifact-admin',
+    role="roles/artifactregistry.admin",
+    members=[pulumi.Output.concat("serviceAccount:", service_account.email)],
+    project=project
+)
+
+document_bucket = gcp.storage.Bucket(
+    'eviction-addresses-documents',
+    location = "us-central1"
+)
+
+image_bucket = gcp.storage.Bucket(
+    'eviction-addresses-images',
+    location = "us-central1"
+)
+
+pulumi.export('document_bucket_name', document_bucket.url)
+pulumi.export('image_bucket_name', image_bucket.url)
+
+
+# Enable Artifact Registry API
+artifact_registry_api = gcp.projects.Service("artifact_registry_api",
+    service="artifactregistry.googleapis.com")
+
+repository = gcp.artifactregistry.Repository(
+    'eviction-addresses-repository',
+    format='DOCKER',
+    repository_id='eviction-addresses-repository',
+    location='us-central1',
+    opts=pulumi.ResourceOptions(depends_on=[artifact_registry_api]),
+)
+
+registry_url = pulumi.Output.concat("us-central1-docker.pkg.dev/", project, "/", repository.repository_id)
+
+api_image_name = pulumi.Output.concat(registry_url, "/eviction-addresses-api").apply(lambda url: f'{url}:latest')
+
+api_image = docker.Image('eviction-addresses-api-image',
+             build = docker.DockerBuildArgs(
+                  context = './inst/docker/',
+                  dockerfile = './inst/docker/api/Dockerfile',
+                  platform = 'linux/x86_64'
+             ),
+             image_name = api_image_name,
+             registry = docker.RegistryArgs(
+                 server = registry_url
+             ),
+             opts=pulumi.ResourceOptions(depends_on=[repository])
+            )
+
+pulumi.export('api_base_image_name', api_image.base_image_name)
+pulumi.export('api_full_image_name', api_image.image_name)
