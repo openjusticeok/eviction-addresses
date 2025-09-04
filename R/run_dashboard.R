a5fc96f6432f878a1a7994006ccc0223dc6738f1
Brancen Gregory <brancengregory@gmail.com>
2025-09-04 15:52:48 -0500

    Update runApp logic

--- R/run_dashboard.R
+++ R/run_dashboard.R
@@ -2,22 +2,23 @@
 #' @description Runs the eviction address entry dashboard
 #'
 #' @param config The path to a config.yml file
-#' @param ... Additional arguments passed to `shiny::shinyApp`
+#' @param ... Additional arguments passed to `shiny::runApp`
 #'
 #' @export
 #'
 run_dashboard <- function(config, ...) {
   logger::log_info('Active Configuration: {Sys.getenv("R_CONFIG_ACTIVE")}')
-  
+
   db <- new_db_pool(config = config)
 
-  shiny::shinyApp(
-    ui = dashboard_ui,
-    server = dashboard_server(db = db, config = config),
-    onStop = function() {
-      pool::poolClose(db)
-      logger::log_info("Database pool closed.")
-    },
-    options = list(...)
-  )
+  tryCatch({
+    app <- shiny::shinyApp(
+      ui = dashboard_ui,
+      server = dashboard_server(db = db, config = config)
+    )
+    shiny::runApp(app, ...)
+  }, finally = {
+    pool::poolClose(db)
+    logger::log_info("Database pool closed.")
+  })
 }
